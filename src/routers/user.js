314b58ed27678414960b38bb2c315161cb3ba1a8
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-11-02 12:12:07 -0700

    Finished routes for creating and loggin in user

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,4 +5,20 @@ const router = new express.Router()
 
 router.post('/createuser', async (req, res) => {
     const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send({ user })
+    } catch (e) {
+        res.status(400).sned(e)
+    }
+})
+
+router.post('/loginuser', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.f_name, req.body.l_name, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
