21385fec3dd634b3740807f4954a2daca544c7b0
David Gaeta <dinkwho@gmail.com>
2019-11-02 13:32:42 -0700

    updated posts.js

--- src/models/posts.js
+++ src/models/posts.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const PostSchema = new Schema({
     title : String,
     body : String,
-    user_info: 
-    tags: {
-
-    }
+    upvote: int,
+    downvote: int
 })
