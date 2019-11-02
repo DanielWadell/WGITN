2d1cd7da25cc635d091cd9faf495fcf8568bfbfa
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-11-02 14:00:28 -0700

    Logout user route added

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
-
+//Creating a new User
 router.post('/createuser', async (req, res) => {
     const user = new User(req.body)
 
@@ -14,12 +14,27 @@ router.post('/createuser', async (req, res) => {
     }
 })
 
+//Loggin in a User
 router.post('/loginuser', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.user_name, req.body.password)
         res.send(user)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+//Logs out the User
+router.get('/logoutuser', async (req, res) => {
+    if (req.session) {
+
+        req.session.destroy((err) => {
+            if(err) {
+                return next(err)
+            } else {
+                return res.redirect('/')
+            }
+        })
     }
 })
 
