16ee66ad080f5ff791dba9965ee8695815f5a49c
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-11-02 14:30:24 -0700

    Added findByCredentials method in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 const Schema = mongoose.Schema;
 
 //Create Schema and Model
@@ -15,6 +16,22 @@ const UserSchema = new Schema({
     }
     
 });
+
+userSchema.statics.findByCredentials = async (user_name, password) => {
+    const user = await User.findOne({ user_name })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
 
 const User = mongoose.model('user', UserSchema);
 
